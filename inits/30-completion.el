bcb75bfc980b96abdae78b4de63b21655c4e2645
Kaz Hosokawa <kazush@gmail.com>
2018-01-02 19:07:50 -0800

    Select a company candidate by pressing a number

--- inits/30-completion.el
+++ inits/30-completion.el
@@ -37,7 +37,31 @@
     (set-face-attribute 'company-tooltip-common nil
                         :inherit 'company-tooltip
                         :background (color-lighten-name bg 20))
-    ))
+    )
+  ;; quoted from https://oremacs.com/2017/12/27/company-numbers/
+  (let ((map company-active-map))
+    (mapc
+     (lambda (x)
+       (define-key map (format "%d" x) 'ora-company-number))
+     (number-sequence 0 9))
+    (define-key map " " (lambda ()
+                          (interactive)
+                          (company-abort)
+                          (self-insert-command 1)))
+    (define-key map (kbd "<return>") nil))
+  (defun ora-company-number ()
+    "Forward to `company-complete-number'.
+
+Unless the number is potentially part of the candidate.
+In that case, insert the number."
+    (interactive)
+    (let* ((k (this-command-keys))
+           (re (concat "^" company-prefix k)))
+      (if (cl-find-if (lambda (s) (string-match re s))
+                      company-candidates)
+          (self-insert-command 1)
+        (company-complete-number (string-to-number k)))))
+  )
 
 (use-package company-ycmd
   :ensure t
