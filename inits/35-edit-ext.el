7631a9eff7d3c36245f9e048e1fba2136be1cad4
Kaz Hosokawa <kazush@gmail.com>
2018-04-02 20:48:29 -0700

    Remove key bindings on mc and add hydra instead

--- inits/35-edit-ext.el
+++ inits/35-edit-ext.el
@@ -6,10 +6,27 @@
 ;; multiple-cursors
 (use-package multiple-cursors
   :ensure t
-  :bind (("C-x r t" . mc/edit-lines)
-         ("C-c >" . mc/mark-next-like-this)
-         ("C-c <" . mc/mark-previous-like-this)
-         ("C-c /" . mc/mark-all-like-this)))
+  :config
+  (defhydra multiple-cursors-hydra (:hint nil)
+    "
+     ^Up^            ^Down^        ^Other^
+----------------------------------------------
+[_p_]   Next    [_n_]   Next    [_l_] Edit lines
+[_P_]   Skip    [_N_]   Skip    [_a_] Mark all
+[_M-p_] Unmark  [_M-n_] Unmark  [_r_] Mark by regexp
+^ ^             ^ ^             [_q_] Quit
+"
+  ("l" mc/edit-lines :exit t)
+  ("a" mc/mark-all-like-this :exit t)
+  ("n" mc/mark-next-like-this)
+  ("N" mc/skip-to-next-like-this)
+  ("M-n" mc/unmark-next-like-this)
+  ("p" mc/mark-previous-like-this)
+  ("P" mc/skip-to-previous-like-this)
+  ("M-p" mc/unmark-previous-like-this)
+  ("r" mc/mark-all-in-region-regexp :exit t)
+  ("q" nil))
+  )
 
 ;; wgrep
 (use-package wgrep :ensure t)
