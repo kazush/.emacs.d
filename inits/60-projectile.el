bcb85d3be7236a7e30d626588385001cff1abc36
Kaz Hosokawa <kazush@gmail.com>
2018-04-25 20:37:39 -0700

    Set projectile-completion-system

--- inits/60-projectile.el
+++ inits/60-projectile.el
@@ -2,10 +2,13 @@
   :ensure t
   :config
   (setq projectile-mode-line '(:eval (format " [%s]" (projectile-project-name))))
-  (projectile-mode))
+  (projectile-mode)
+  (setq projectile-completion-system 'helm))
+
 (use-package helm-projectile
   :ensure t
   :config (helm-projectile-on))
+
 (use-package projectile-speedbar
   :ensure t
   :bind ("C-c I p" . projectile-speedbar-open-current-buffer-in-tree))
