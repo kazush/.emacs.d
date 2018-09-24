88c40c95820aecf05fb4786a82cafe8976dc0933
Kaz Hosokawa <kazush@gmail.com>
2018-09-23 19:47:54 -0700

    Rework company tooltip colors

--- inits/30-completion.el
+++ inits/30-completion.el
@@ -23,23 +23,52 @@
   (add-hook 'eshell-mode-hook (lambda () (setq-local company-idle-delay 0.5)))
   (setq company-echo-delay 0)
   (setq company-dabbrev-downcase nil)
-  (require 'color)
-  (let ((bg (face-attribute 'default :background)))
-    (set-face-attribute 'company-tooltip nil
-                        :inherit 'default
-                        :background (color-lighten-name bg 20))
-    (set-face-attribute 'company-scrollbar-bg nil
-                        :background (color-lighten-name bg 20))
-    (set-face-attribute 'company-scrollbar-fg nil
-                        :background (color-lighten-name bg 10))
-    (set-face-attribute 'company-tooltip-selection nil
-                        :inherit 'company-tooltip)
-    (set-face-attribute 'company-tooltip-common-selection nil
-                        :inherit 'company-tooltip)
-    (set-face-attribute 'company-tooltip-common nil
-                        :inherit 'company-tooltip
-                        :background (color-lighten-name bg 20))
-    )
+  (setq company-tooltip-align-annotations t)
+  (defun my/set-company-tooltip-faces ()
+    "Set faces for company tooltip."
+    (interactive)
+    (let ((bg (face-attribute 'default :background)))
+      (set-face-attribute 'company-tooltip nil
+                          :background (color-lighten-name
+                                       (face-background 'default)
+                                       10)
+                          :inherit 'default)
+      (set-face-attribute 'company-tooltip-selection nil
+                          :foreground 'unspecified
+                          ;; :background (color-lighten-name
+                          ;;              (face-background 'company-tooltip)
+                          ;;              10)
+                          :background "steelblue"
+                          :inherit 'company-tooltip)
+      (set-face-attribute 'company-tooltip-common nil
+                          :background 'unspecified
+                          :inherit 'company-tooltip)
+      (set-face-attribute 'company-tooltip-common-selection nil
+                          :foreground 'unspecified
+                          :background (face-background
+                                       'company-tooltip-selection)
+                          :inherit 'company-tooltip-common)
+      (set-face-attribute 'company-tooltip-annotation nil
+                          :foreground "LightSlateBlue"
+                          :background 'unspecified
+                          :inherit 'company-tooltip)
+      (set-face-attribute 'company-tooltip-annotation-selection nil
+                          :foreground 'unspecified
+                          :background (face-background
+                                       'company-tooltip-selection)
+                          :inherit 'company-tooltip-align-annotations)
+      (set-face-attribute 'company-scrollbar-bg nil
+                          :background (color-lighten-name bg 10))
+      (set-face-attribute 'company-scrollbar-fg nil
+                          :background (color-lighten-name bg 40))
+      (set-face-attribute 'company-template-field nil
+                          :foreground (face-foreground
+                                       'company-tooltip-annotation)
+                          :background 'unspecified
+                          :slant 'unspecified
+                          :inherit 'default)
+      ))
+  (add-hook 'emacs-startup-hook 'my/set-company-tooltip-faces)
   ;; quoted from https://oremacs.com/2017/12/27/company-numbers/
   (let ((map company-active-map))
     (mapc
