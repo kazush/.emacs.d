9d7071b841661035ca3ebc8731d0e024fe283e96
Kaz Hosokawa <kazush@gmail.com>
2018-10-06 11:02:52 -0700

    Diminish highlight-indent-guides-mode

--- inits/35-prog-ext.el
+++ inits/35-prog-ext.el
@@ -135,5 +135,6 @@
 (use-package highlight-indent-guides
   :ensure t
   :hook (prog-mode . highlight-indent-guides-mode)
+  :diminish highlight-indent-guides-mode
   :config
   (setq highlight-indent-guides-method 'character))
