46ee2c4db8f472cd8509346a71839eaca0d4f00a
Kaz Hosokawa <kazush@gmail.com>
2022-07-20 19:54:05 +0900

    add straight.el

---
+++ early-init.el
@@ -0,0 +1,2 @@
+(setq package-enable-at-startup nil)
+(setq straight-check-for-modifications '(check-on-save find-when-checking))
--- init.el
+++ init.el
@@ -1,16 +1,33 @@
-(require 'package)
-(require 'gnutls)
-(setq package-archives
-      '(("gnu" . "https://elpa.gnu.org/packages/")
-        ;; ("melpa-stable" . "https://stable.melpa.org/packages/")
-        ("melpa" . "https://melpa.org/packages/")))
-(setq package-user-dir (concat "~/.emacs.d/" emacs-version "/elpa"))
-(setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
-(package-initialize)
+;; Use straight.el for package management.
+
+(setq straight-base-dir (concat "~/.emacs.d/" emacs-version))
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" straight-base-dir))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+;; Comment out package.el related settings.
+;; (require 'package)
+;; (require 'gnutls)
+;; (setq package-archives
+;;       '(("gnu" . "https://elpa.gnu.org/packages/")
+;;         ;; ("melpa-stable" . "https://stable.melpa.org/packages/")
+;;         ("melpa" . "https://melpa.org/packages/")))
+;; (setq package-user-dir (concat "~/.emacs.d/" emacs-version "/elpa"))
+;; (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
+;; (package-initialize)
 
 (setq custom-file (concat "~/.emacs.d/" emacs-version "/custom.el"))
 (when (file-exists-p custom-file)
   (load custom-file))
 
-(require 'org)
+(straight-use-package '(org :type built-in))
 (org-babel-load-file (expand-file-name "~/.emacs.d/emacs-init.org"))
