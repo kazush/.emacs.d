b51fba05dd1064ad869e9fd1af5f78b1cfdbae93
Kaz Hosokawa <kazush@gmail.com>
2024-05-18 13:23:29 +0900

    add GUI settings to early-init for reducing startup time

--- early-init.el
+++ early-init.el
@@ -1,2 +1,9 @@
+;; For straight.
 (setq package-enable-at-startup nil)
 (setq straight-check-for-modifications '(check-on-save find-when-checking))
+
+;; UI settings.
+(push '(menu-bar-lines . 0) default-frame-alist)
+(push '(tool-bar-lines . 0) default-frame-alist)
+
+(setq frame-inhibit-implied-resize t)
