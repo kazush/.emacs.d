851fd46bd4db303683d8195819760e8ca1b0457b
Kaz Hosokawa <kazush@gmail.com>
2024-05-19 14:47:47 +0900

    add more default frame settings

--- early-init.el
+++ early-init.el
@@ -5,5 +5,9 @@
 ;; UI settings.
 (push '(menu-bar-lines . 0) default-frame-alist)
 (push '(tool-bar-lines . 0) default-frame-alist)
+(push '(background-color . "gray10") default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
+(push '(left-fringe . 1) default-frame-alist)
+(push '(right-fringe . 1) default-frame-alist)
 
 (setq frame-inhibit-implied-resize t)
