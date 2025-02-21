fc8797819bcb66204edb0b8c8bcba6d019588b1e
Kaz Hosokawa <kazush@gmail.com>
2025-02-21 23:51:29 +0900

    fix: refactor font init code for improved terminal compatibility

--- early-init.el
+++ early-init.el
@@ -1,6 +1,45 @@
-
 ;; (require 'profiler)
 ;; (profiler-start 'cpu)
+
+;; For measurement of startup time.
+
+(defconst my/before-load-init-time (current-time))
+
+;;;###autoload
+(defun my/load-init-time ()
+  "Loading time of user init files including time for `after-init-hook'."
+  (let ((time1 (float-time
+                (time-subtract after-init-time my/before-load-init-time)))
+        (time2 (float-time
+                (time-subtract (current-time) my/before-load-init-time))))
+    (message (concat "Loading init files: %.0f [msec], "
+                     "of which %.f [msec] for `after-init-hook'.")
+             (* 1000 time1) (* 1000 (- time2 time1)))))
+(add-hook 'after-init-hook #'my/load-init-time t)
+
+(defvar my/tick-previous-time my/before-load-init-time)
+
+;;;###autoload
+(defun my/tick-init-time (msg)
+  "Tick boot sequence at loading MSG."
+  (when my/loading-profile-p
+    (let ((ctime (current-time)))
+      (message "---- %5.2f[ms] %s"
+               (* 1000 (float-time
+                        (time-subtract ctime my/tick-previous-time)))
+               msg)
+      (setq my/tick-previous-time ctime))))
+
+(defun my/emacs-init-time ()
+  "Emacs booting time in msec."
+  (interactive)
+  (message "Emacs booting time: %.0f [msec] = `emacs-init-time'."
+           (* 1000
+              (float-time (time-subtract
+                           after-init-time
+                           before-init-time)))))
+
+(add-hook 'after-init-hook #'my/emacs-init-time)
 
 ;; GC settings.
 (setq gc-cons-threshold (* 100 1024 1024))
@@ -18,3 +57,21 @@
       inhibit-x-resources t
       inhibit-startup-echo-area-message user-login-name
       inhibit-startup-buffer-menu t)
+
+;;
+;; Initialize startup frame to minimize flicker.
+;;
+
+(set-face-attribute 'default nil
+                    :foreground "#abb2bf"
+                    :background "#282c34")
+(set-face-attribute 'mode-line nil
+                    :foreground "#d3d3d3"
+                    :background "#000000")
+
+(when window-system
+  (scroll-bar-mode -1)
+  (tool-bar-mode -1)
+  (menu-bar-mode -1)
+  (fringe-mode 10)
+  (set-frame-size nil 100 35))
